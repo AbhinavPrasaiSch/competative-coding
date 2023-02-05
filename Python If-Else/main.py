adcc0e86cc4a5a349ee06d4661a94961e7bb5b79
AbhinavPrasaiSch <122995648+AbhinavPrasaiSch@users.noreply.github.com>
2023-02-05 09:20:03 -0800

    Add files via upload

---
+++ Python If-Else/main.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import math
+import sys
+
+n = input()
+if n % 2 != 0:
+    print("Weird")
+elif n % 2 == 0 and n in range(2, 6):
+    print("Not Weird")    
+elif n % 2 == 0 and n in range(6, 21):
+    print("Weird")
+else:
+    print("Not Weird")
